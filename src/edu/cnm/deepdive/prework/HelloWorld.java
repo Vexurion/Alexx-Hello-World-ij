948a69a968b169041e44ae53d95406e862fc13ec
Alexx <Vexurion@gmail.com>
2019-12-09 11:27:43 -0700

    Add salutation method to construct and return a salutation string.

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -4,21 +4,13 @@ public class HelloWorld {
 
 
   public static void main(String[] args) {
-    salute((args.length > 0) ? args[0] : "World");
-
-    //Using "main" to invoke "salute"
-//    salute(target: "World")
-
-    //     This was a simple if else control statement
-//    if (args.length > 0)  {
-//
-//      salute(args[0]);
-//    } else {
-//      salute("World");
-//    }
+    salute(salutation(args));
   }
-  private static void salute(String target) {
-    //Example of String Concatenation (combines multiple strings into single string)
-    System.out.println("Hello, " + target + "!");
+
+  private static String salutation(String[] args) {
+    return "Hello, " + ((args.length > 0) ? args[0] : "World");
+  }
+  private static void salute(String salutation) {
+    System.out.println(salutation);
   }
 }
