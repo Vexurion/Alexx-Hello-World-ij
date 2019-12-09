09b19b5c84c5c5c461438c8d97a0c7fdf7822269
Alexx <Vexurion@gmail.com>
2019-12-09 10:53:55 -0700

    Add salute method and test for command line arguments.

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -1,8 +1,24 @@
 package edu.cnm.deepdive.prework;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
 
+
+  public static void main(String[] args) {
+    salute((args.length > 0) ? args[0] : "World");
+
+    //Using "main" to invoke "salute"
+//    salute(target: "World")
+
+    //     This was a simple if else control statement
+//    if (args.length > 0)  {
+//
+//      salute(args[0]);
+//    } else {
+//      salute("World");
+//    }
+  }
+  private static void salute(String target) {
+    //Example of String Concatenation (combines multiple strings into single string)
+    System.out.println("Hello, " + target + "!");
+  }
 }
